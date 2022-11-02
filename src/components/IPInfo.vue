eb2ea2a8f6a39513e9f918ce8e106ee9bc4912d4
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2022-11-03 01:55:44 +0200

    dynamic info

--- src/components/IPInfo.vue
+++ src/components/IPInfo.vue
@@ -5,26 +5,28 @@
     <!-- Flex Item #1 -->
     <div class="flex flex-col">
       <h3 class="text-xs mb-2 uppercase">IP Address</h3>
-      <span class="text-2xl">12321321</span>
+      <span class="text-2xl">{{ipInfo.address}}</span>
     </div>
     <!-- Flex Item #2 -->
     <div class="flex flex-col">
       <h3 class="text-xs mb-2 uppercase">Location</h3>
-      <span class="text-2xl">12321321</span>
+      <span class="text-2xl">{{ipInfo.state}}</span>
     </div>
     <!-- Flex Item #3 -->
     <div class="flex flex-col">
       <h3 class="text-xs mb-2 uppercase">Time Zone</h3>
-      <span class="text-2xl">12321321</span>
+      <span class="text-2xl">UTC {{ipInfo.timezone}}</span>
     </div>
        <!-- Flex Item #4 -->
        <div class="flex flex-col">
         <h3 class="text-xs mb-2 uppercase">ISP</h3>
-        <span class="text-2xl">12321321</span>
+        <span class="text-2xl">{{ipInfo.isp}}</span>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+    props: ['ipInfo']
+};
 </script>
