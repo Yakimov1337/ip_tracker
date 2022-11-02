e3a2b03011f7f6873d4b397dd49d106f1962ceea
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2022-11-02 20:16:36 +0200

    Search input and Ip info added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,35 @@
 <template>
-    <div class="flex flex-col h-screen max-h-screen">
-        <!-- Search / Results -->
-        <div class="flex justify-center relative bg-hero-pattern bg-cover px-4 pt-8 pb-32">
-            <!-- Search Input    -->
-            <div class="w-full mx-w-screen-sm" >
-                <h1 class="text-white text-center text-3xl pb-4">IP Address Tracker</h1>
-                <div class="flex">
-                    <input class="flex-1 py-3 px-2 rounded-tl-md rounded-bl-md focus:outline-none" 
-                    type="text"
-                    placeholder="Search for any IP address or leave empty to get your ip info"
-                    />
-
-                 
-                </div>
-            </div>
+  <div class="flex flex-col h-screen max-h-screen">
+    <!-- Search / Results -->
+    <div
+      class="flex justify-center relative bg-hero-pattern bg-cover px-4 pt-8 pb-32"
+    >
+      <!-- Search Input    -->
+      <div class="w-full max-w-screen-sm">
+        <h1 class="text-white text-center text-3xl pb-4">IP Address Tracker</h1>
+        <div class="flex">
+          <input
+            class="flex-1 py-3 px-2 rounded-tl-md rounded-bl-md focus:outline-none"
+            type="text"
+            placeholder="Search for any IP address or leave empty to get your ip info"
+          />
+          <i
+            class="cursor-pointer bg-black text-white px-4 rounded-tr-md rounded-br-md flex items-center fas fa-chevron-right"
+          ></i>
         </div>
+      </div>
+
+      <!-- IP Info -->
+      <IPInfo/>
     </div>
+  </div>
 </template>
 
 <script>
-export default {
-    name:"Home",
-    components: {
+import IPInfo from "@/components/IPInfo.vue";
 
-    },
+export default {
+  name: "Home",
+  components: { IPInfo },
 };
 </script>
